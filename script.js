63828b1c045d2219cc4b041022aff2c012ea07ae
Jean Carlos Franzoi Dernis <105512458+jeanfranzoi@users.noreply.github.com>
2026-07-07 13:51:20 -0300

    Implement popup window and button event handlers.
    Added functions to open a popup window and handle close/back button events.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+let popUpWin = null;
+
+function abrirJanela(url,left,top,width,height){
+    if(popUpWin && !popUpWin.closed){
+        popUpWin.close();
+    }
+
+    popUpWin = window.open(
+        url,
+        "popUpWin",
+        "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width="+width+",height="+height+",left="+left+",top="+top
+    );
+}
+
+// ======================
+// eventos automáticos
+// ======================
+document.addEventListener("DOMContentLoaded", function () {
+
+// ----------------------
+// botão fechar janela
+// ----------------------
+    const botoesFechar = document.querySelectorAll(".btn-fechar");
+    botoesFechar.forEach(btn => {
+        btn.addEventListener("click", function(){
+            window.close();
+        });
+    });
+
+// ----------------------
+// botão fechar janela
+// ----------------------
+
+const botoesVoltar = document.querySelectorAll(".btn-voltar");
+    botoesVoltar.forEach(btn => {
+        btn.addEventListener("click", function(){
+            history.back();
+        });
+    });
 
